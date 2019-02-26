3f71bfc2199622c1418d236c390bbd624b4c1740
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2019-02-26 11:07:57 -0500

    Run migrations during Docker run script (#4)

--- docker/run-local.sh
+++ docker/run-local.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+set -e
 
+# Setup the database (run migrations)
+docker run --rm -v "$(pwd)":"/hello-cammis-data" ca-mmis/hello-cammis-data python manage.py migrate
+
+# Run the application server
 docker run -it --rm -p 8000:8000 -v "$(pwd)":"/hello-cammis-data" ca-mmis/hello-cammis-data
